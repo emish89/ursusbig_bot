939782cb4a076deef15a70cc589984becef91422
fballarini <emish89@yahoo.it>
2022-10-25 18:19:24 +0200

    fix(docs): fix typo

--- app/docs.ts
+++ app/docs.ts
@@ -6,7 +6,8 @@ export const docs = async (chat: Chat) => {
   sendMessage(
     chat.id,
     "Ecco qui il link a tutta la documentazione, abbiamo pinnato il messaggio nella conversazione: LINK "
-  // ).then(async (res) => {
+  );
+  // .then(async (res) => {
   //   const messageId = res.data.result.message_id;
   //   //pin message
   //   await pinChatMessage(chat.id, messageId);
