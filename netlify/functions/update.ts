6fb79b4954821cb931d16bb4c4d426e82f960fca
fballarini <emish89@yahoo.it>
2022-10-25 18:00:30 +0200

    chore(test): finish log

--- netlify/functions/update.ts
+++ netlify/functions/update.ts
@@ -52,6 +52,7 @@ const handler: Handler = async (event: Event) => {
     jsonBody.message.chat,
     commandArguments[commandKeys[0]]
   );
+  console.log("finish");
   return {
     statusCode: 200,
   };
